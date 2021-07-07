8564f1a74291130fa2196d596a8692d16f7df12f
Farrukh Jamal <52830299+farrukh806@users.noreply.github.com>
2021-07-07 16:03:40 +0530

    Added all files.

---
+++ src/classes/Invoice.ts
@@ -0,0 +1,18 @@
+import HasFormatter from "../interfaces/hasFormatter.js";
+
+class Invoice implements HasFormatter {
+    readonly client:string;
+    details:string;
+    amount:number;
+    constructor(c:string, d:string,a:number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        return `${this.client} owes £${this.amount} for ${this.details}`
+    }
+}
+
+export default Invoice;
